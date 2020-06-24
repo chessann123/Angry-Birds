8e3b9be3d35debde3e510f6fafc2c44f0e531ac7
chessann123 <55215674+chessann123@users.noreply.github.com>
2020-06-23 19:28:06 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,35 +3,51 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground, ball;
+var box1, box2, box3, box4, box5;
+var log1, log2, log3, log4;
+var pig1, pig2;
+var bird, ground;
 
 function setup(){
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    var ground_options ={
-        isStatic: true
-    }
+    ground = new Ground(600,390,1200,20);
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810,350);
+    log1 = new Log(810,260,300,PI/2);
 
-    var ballOptions = {
-        restitution : 1
-    }
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2 = new Pig(810,220);
+    log2 = new Log(810,180,300,PI/2);
 
-    ball = Bodies.circle(200,100,30, ballOptions);
-    World.add (world, ball);
+    box5 = new Box(810,160,70,70);
+    log3 = new Log(760,120,150,PI/5);
+    log4 = new Log(870,120,150,-PI/5);
 
-    console.log(ground);
+    bird = new Bird(100, 100);
+
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x,ball.position.y,30,30);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
+    box1.display();
+    ground.display();
+    box2.display();
+    pig1.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+    box5.display();
+    log3.display();
+    log4.display();
+    bird.display();
+   
 }
